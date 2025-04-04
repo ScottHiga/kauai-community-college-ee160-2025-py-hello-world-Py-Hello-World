1f6314bd1d13cbd875ea0aeac55d7bab1eb09c11
ScottHiga <scottyh@hawaii.edu>
2025-04-03 17:12:11 -1000

    Add files via upload

---
+++ HIGA_HelloWorld.py
@@ -0,0 +1,11 @@
+#########################################################################################
+#HIGA_HelloWorld.py prints the text "Hello World!" to the display
+#Created by Scott Higa: Feb 21, 2025
+#Last Edit: April 3, 2025
+#########################################################################################
+# print (lowercase p) is a built-in function in python that displays the argument within the parenthesis that follows.
+# types of arguments we've learned to this point: int, float, str
+# Python will read the string "Hello World!" and display it to the output
+# The print () function will always display a string
+
+print ("Hello World!")
